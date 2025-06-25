94720525447d12b58eb633cf80f06382fc6b87e1
jkbsnl <jakob.snel@t-online.de>
2025-06-25 15:35:27 +0200

    refinement

--- config.py
+++ config.py
@@ -38,19 +38,19 @@ QUESTION_SCALE_MAP = {
 
 EXAMPLE_IMAGES = {
     'DeepLift': [
-        ('assets/n03095699/DeepLift/0.jpg', 'The majority of importance is assigned to the space between bridge and ship, making it not aligned very well.'), #bridge
-        ('assets/n01695060/DeepLift/0.jpg', 'The same applies here, yet one can argue, that for the komodo dragon, human attention is lead towards the **ground truth** here.') #komodo dragon
+        ('assets/n03095699/DeepLift/0.jpg', 'The majority of importance is assigned to the space between bridge and ship, making it not aligned very well. Also, this area does not contain information, that would tell us as humans that a container ship might be present in the image.'), #bridge
+        ('assets/n01695060/DeepLift/0.jpg', 'The same applies here, yet one can argue that for the komodo dragon, human attention is lead towards the **ground truth** here (due to the shape).') #komodo dragon
     ],
     'GradCAM': [
-        ('assets/n03095699/GradCAM/0.jpg', 'Even though medium importance is assigned to the bridge (right), the majority of importance is assigned to the ship.'), 
-        ('assets/n01695060/GradCAM/0.jpg', 'Even better focus on the **ground truth**. Important note: Artifacts like on the right (0 importance) can be ignored for the survey, if not in relevant parts of the image.')
+        ('assets/n03095699/GradCAM/0.jpg', 'Even though medium importance is assigned to the bridge (right), the majority of importance is assigned to the ship, making this a more intuituve explanation.'), 
+        ('assets/n01695060/GradCAM/0.jpg', 'Even better focus on the **ground truth**. Important note: Artifacts like on the right (grey, 0 importance) can be ignored for the survey, if not in relevant parts of the image.')
     ],
     'GuidedBackprop': [
         ('assets/n03095699/GuidedBackprop/0.jpg', 'Significant importance is assigned to the bridge. For question 2, this requires You to decide, if a bridge is relevant for classifying the ground truth object as a container ship, or not.'),
-        ('assets/n01695060/GuidedBackprop/0.jpg', 'Again, some importance on the background, but to smaller extent.')
+        ('assets/n01695060/GuidedBackprop/0.jpg', 'Again, some importance on the background, but to a smaller extent. In contrast, the majority of importance is assigned to the komodo dragon, which is the ground truth object.')
     ],
     'LRP_EpsilonAlpha2Beta1': [
-        ('assets/n03095699/LRP_EpsilonAlpha2Beta1/0.jpg', 'Almost perfect alignment with the ground truth object.'),
+        ('assets/n03095699/LRP_EpsilonAlpha2Beta1/0.jpg', 'Almost perfect alignment with the ground truth object, even though it is not fully covered/ aligned (Not technically bad if explanation is intuitively leading to the ground truth).'),
         ('assets/n01695060/LRP_EpsilonAlpha2Beta1/0.jpg', 'Same here.')
     ]     
 }
